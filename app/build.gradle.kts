305a44a2a28c61ed624181dbf7a70749b4e89692
eirikjg46 <eirikjg@hiof.no>
2023-10-05 14:56:56 +0200

    Musikkspiller demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -56,6 +57,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-inappmessaging:20.3.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/definitely_not_spotify/MainActivity.kt
+++ app/src/main/java/com/example/definitely_not_spotify/MainActivity.kt
@@ -2,42 +2,133 @@ package com.example.definitely_not_spotify
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.definitely_not_spotify.ui.theme.DefinitelyNotSpotifyTheme
+import android.media.MediaPlayer
+import android.os.Handler
+
+import android.widget.Toast
+import android.widget.SeekBar
+import com.example.definitely_not_spotify.databinding.LayoutBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var runnable: Runnable
+    private var handler: Handler = Handler()
+    private var pause: Boolean = false
+    private lateinit var layoutBind: LayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DefinitelyNotSpotifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+        layoutBind = LayoutBinding.inflate(layoutInflater)
+        val view = layoutBind.root
+
+        setContentView(view)
+
+        // Start the media player
+
+
+        layoutBind.playBtn.setOnClickListener {
+            if (pause) {
+                mediaPlayer.seekTo(mediaPlayer.currentPosition)
+                mediaPlayer.start()
+                pause = false
+                Toast.makeText(this, "media playing", Toast.LENGTH_SHORT).show()
+            } else {
+
+                mediaPlayer = MediaPlayer.create(applicationContext, R.raw.nggyu)
+                mediaPlayer.start()
+                Toast.makeText(this, "media playing", Toast.LENGTH_SHORT).show()
+
+            }
+            initializeSeekBar()
+            layoutBind.playBtn.isEnabled = false
+            layoutBind.pauseBtn.isEnabled = true
+            layoutBind.stopBtn.isEnabled = true
+
+            mediaPlayer.setOnCompletionListener {
+                layoutBind.playBtn.isEnabled = true
+                layoutBind.pauseBtn.isEnabled = false
+                layoutBind.stopBtn.isEnabled = false
+                Toast.makeText(this, "end", Toast.LENGTH_SHORT).show()
+
             }
         }
+
+        // Pause the media player
+        layoutBind.pauseBtn.setOnClickListener {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+                pause = true
+                layoutBind.playBtn.isEnabled = true
+                layoutBind.pauseBtn.isEnabled = false
+                layoutBind.stopBtn.isEnabled = true
+                Toast.makeText(this, "media pause", Toast.LENGTH_SHORT).show()
+            }
+        }
+        // Stop the media player
+        layoutBind.stopBtn.setOnClickListener {
+            if (mediaPlayer.isPlaying || pause.equals(true)) {
+                pause = false
+                layoutBind.seekBar.setProgress(0)
+                mediaPlayer.stop()
+                mediaPlayer.reset()
+                mediaPlayer.release()
+                handler.removeCallbacks(runnable)
+
+                layoutBind.playBtn.isEnabled = true
+                layoutBind.pauseBtn.isEnabled = false
+                layoutBind.stopBtn.isEnabled = false
+                layoutBind.tvPass.text = ""
+                layoutBind.tvDue.text = ""
+                Toast.makeText(this, "media stop", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Seek bar change listener
+        layoutBind.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                if (b) {
+                    mediaPlayer.seekTo(i * 1000)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
+    }
+
+    // Method to initialize seek bar and audio stats
+    fun initializeSeekBar() {
+        layoutBind.seekBar.max = mediaPlayer.seconds
+
+        runnable = Runnable {
+            layoutBind.seekBar.progress = mediaPlayer.currentSeconds
+
+            layoutBind.tvPass.text = "${mediaPlayer.currentSeconds} sec"
+            val diff = mediaPlayer.seconds - mediaPlayer.currentSeconds
+            layoutBind.tvDue.text = "$diff sec"
+
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DefinitelyNotSpotifyTheme {
-        Greeting("Android")
+// Creating an extension property to get the media player time duration in seconds
+val MediaPlayer.seconds: Int
+    get() {
+        return this.duration / 1000
     }
-}
+// Creating an extension property to get media player current position in seconds
+val MediaPlayer.currentSeconds: Int
+    get() {
+        return this.currentPosition / 1000
+    }
+
+
+
+
+
+
