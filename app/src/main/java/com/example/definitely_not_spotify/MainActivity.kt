1d97ece8a2e3e95724272ab3aa4f7f4911dd4eaa
bragers <bragers@hiof.no>
2023-10-12 12:24:31 +0200

    EnkelNavigation

--- app/src/main/java/com/example/definitely_not_spotify/MainActivity.kt
+++ app/src/main/java/com/example/definitely_not_spotify/MainActivity.kt
@@ -3,30 +3,13 @@ package com.example.definitely_not_spotify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.definitely_not_spotify.ui.theme.DefinitelyNotSpotifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    DefNotSpotifyApp()
                 }
             }
         }
@@ -46,65 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(message: String,
-               to: String,
-               modifier: Modifier = Modifier) {
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
         modifier = modifier
-    ){
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(top = 16.dp),
-        )
-        Text(
-            text = to,
-            fontSize = 36.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .padding(end = 16.dp)
-        )
-        Button(onClick = { /*TODO*/ }) {
-            Text(stringResource(R.string.switchScreen))
-        }
-    }
+    )
 }
 
-@Composable
-fun Images(message: String,
-           to: String,
-           modifier: Modifier = Modifier) {
-    Box(modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.pancakes),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        HomeScreen(
-            message = message,
-            to = to,
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.Center)
-                .padding(8.dp)
-        )
-
-    }
-}
-
-
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DefinitelyNotSpotifyTheme {
-        Images(
-            message = stringResource(R.string.home_screen),
-            to = stringResource(R.string.welcome_to_definitelynotspotify))
+        Greeting("Android")
     }
 }
